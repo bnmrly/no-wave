90d6b7ba64f9e8b7e4473b6a0d0958ace0c0554b
Ben Marley <benjaminjohnmarley@gmail.com>
2017-10-16 18:47:05 +0100

    white onmouseover for #spanish

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-// document.getElementById("demo").innerHTML = "NEU!";
+// Displays font white on onmouseover and black onmouseleave for #spanish
+
+document.getElementById("spanish").onmouseover = function () { mouseOver()}
+
+function mouseOver () {
+  document.getElementById("spanish").style.color="white";
+}
+
+document.getElementById("spanish").onmouseleave = function () { mouseLeave()}
+function mouseLeave () {document.getElementById("spanish").style.color="black";
+ }
