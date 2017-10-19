8b00ea362407ef1b1bc27c1de22db927d9c0ea5a
Ben Marley <benjaminjohnmarley@gmail.com>
2017-10-19 17:00:58 +0100

    js rewrite - add this and work with visibility - works but shaky

--- js/main.js
+++ js/main.js
@@ -1,34 +1,9 @@
-// Shows ALL elements as white on mouseover and black onmouseover
-
-// return an array called whitened of the elements with the class of language
-
-var whitened = document.getElementsByClassName("language");
-
- // Create a function that loops through the array and 'whitens' all of the array elements
-
-var displayWhite = function () {
-  for (var i = 0; i < whitened.length; i++) {
-   whitened[i].style.color= 'white';
-  }
+function displayWhite(el) {
+ // console.log(el.id + "This is onmouseover");
+  el.style.visibility="hidden";
  }
 
- // call the function
-
- displayWhite ();
-
- // return an array called blackened of the elements with the class of language
-
- var blackened = document.getElementsByClassName("language");
-
-
-  // Create a function that loops through the array and 'blackens' all of the array elements
-
-  var displayBlack = function () {
-    for (var i = 0; i < blackened.length; i++) {
-     blackened[i].style.color= 'black';
-    }
-   }
-
-    // call the function
-
-    displayBlack ();
+function displayBlack(el) {
+  //  console.log(el.id + "This is onmouseout")
+  el.style.visibility="visible";
+ }
