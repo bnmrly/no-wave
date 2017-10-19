776b606efaed4638ede168ab56be92db857ca89b
Ben Marley <benjaminjohnmarley@gmail.com>
2017-10-19 19:23:25 +0100

    Remove comments and unused class

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
 function displayWhite(el) {
- // console.log(el.id + "This is onmouseover");
-  // el.style.visibility="hidden";
   el.style.color="white";
  }
 
 function displayBlack(el) {
-  //  console.log(el.id + "This is onmouseout")
-  // el.style.visibility="visible";
     el.style.color="black";
  }
