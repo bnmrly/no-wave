b174f8d795735dde9079536df9e306850956d37d
Ben Marley <benjaminjohnmarley@gmail.com>
2017-10-19 17:15:46 +0100

    moved event to p. js color. css start off black.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 function displayWhite(el) {
  // console.log(el.id + "This is onmouseover");
-  el.style.visibility="hidden";
+  // el.style.visibility="hidden";
+  el.style.color="white";
  }
 
 function displayBlack(el) {
   //  console.log(el.id + "This is onmouseout")
-  el.style.visibility="visible";
+  // el.style.visibility="visible";
+    el.style.color="black";
  }
